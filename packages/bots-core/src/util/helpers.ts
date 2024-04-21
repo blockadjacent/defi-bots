3862e5f0000a85fd31abc66a678dab000f42b059
Owan Hunte <owan@blockadjacent.com>
2024-04-21 19:12:18 -0400

    refactor: add prettier dependency and update config

--- packages/bots-core/src/util/helpers.ts
+++ packages/bots-core/src/util/helpers.ts
@@ -1,11 +1,11 @@
 import BigNumber from "bignumber.js";
 
-export const numberToBigNumber = (num: number | string | BigNumber | BigInt): BigNumber => {
-  if (typeof num === "string") {
-    return BigNumber(num.replace(/,/g, ""));
-  } else if (typeof num === "bigint" || num instanceof BigInt) {
-    return BigNumber(num.toString());
-  } else {
-    return BigNumber(num);
-  }
+export const numberToBigNumber = (num: number | string | BigNumber | bigint): BigNumber => {
+    if (typeof num === "string") {
+        return BigNumber(num.replace(/,/g, ""));
+    } else if (typeof num === "bigint") {
+        return BigNumber(num.toString());
+    } else {
+        return BigNumber(num);
+    }
 };
