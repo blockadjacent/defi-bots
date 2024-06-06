168c1e1c46675c1fb4f323c94e36ef5b05af4cad
Owan Hunte <owan@blockadjacent.com>
2024-06-06 14:00:32 -0400

    feat: add migration, seeder and DB tests for algo_assets table

---
+++ supabase/tests/database/algo_assets.test.sql
@@ -0,0 +1,28 @@
+begin;
+select plan(13);
+
+select has_table('algo_assets');
+
+select has_column('algo_assets', 'id');
+select has_column('algo_assets', 'token_id');
+select has_column('algo_assets', 'name');
+select has_column('algo_assets', 'unit_name');
+select has_column('algo_assets', 'network');
+select has_column('algo_assets', 'decimals');
+select has_column('algo_assets', 'is_native');
+select has_column('algo_assets', 'is_supported');
+select has_column('algo_assets', 'created_at');
+select has_column('algo_assets', 'updated_at');
+
+select col_is_pk('algo_assets', 'id');
+
+select policies_are(
+    'public',
+    'algo_assets',
+    ARRAY [
+        'Enable read access for all users'
+    ]
+);
+
+select * from finish();
+rollback;
