e35a466d33b749c1bb0eec90a68aa40d4a8abba4
Owan Hunte <owan@blockadjacent.com>
2024-06-07 19:59:31 -0400

    feat: add helpers in bots-core to get DB client connections

---
+++ packages/bots-core/src/util/db-clients.ts
@@ -0,0 +1,28 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../supabase.types";
+
+export const supabaseAnon = createClient<Database>(
+    process.env.SUPABASE_URL || "",
+    process.env.SUPABASE_ANON_SERVICE_ROLE_KEY || "",
+    {
+        db: {
+            schema: "public",
+        },
+        auth: {
+            persistSession: false,
+        },
+    }
+);
+
+export const supabaseAdmin = createClient<Database>(
+    process.env.SUPABASE_URL || "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY || "",
+    {
+        db: {
+            schema: "public",
+        },
+        auth: {
+            persistSession: false,
+        },
+    }
+);
