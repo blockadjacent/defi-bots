c8eb119099f864340b4ca8090e05dafe19a4dad2
Owan Hunte <owan@blockadjacent.com>
2024-06-09 18:16:04 -0400

    refactor: revert bots-core's db-clients exports to constants

--- packages/bots-core/src/index.ts
+++ packages/bots-core/src/index.ts
@@ -1,4 +1,4 @@
 export type { Json, Database, Enums, Tables, TablesInsert, TablesUpdate } from "./supabase.types";
 
 export { numberToBigNumber, getScaledAmount, getUnScaledAmount } from "./util/helpers";
-export { getSupabaseAnonClient, getSupabaseAdminClient } from "./util/db-clients";
+export { supabaseAnon, supabaseAdmin } from "./util/db-clients";
--- packages/bots-core/src/util/db-clients.ts
+++ packages/bots-core/src/util/db-clients.ts
@@ -1,28 +1,28 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../supabase.types";
 
-export const getSupabaseAnonClient = () => {
-    return createClient<Database>(
-        process.env.SUPABASE_API_URL || "",
-        process.env.SUPABASE_ANON_SERVICE_ROLE_KEY || "",
-        {
-            db: {
-                schema: "public",
-            },
-            auth: {
-                persistSession: false,
-            },
-        }
-    );
-};
-
-export const getSupabaseAdminClient = () => {
-    return createClient<Database>(process.env.SUPABASE_API_URL || "", process.env.SUPABASE_SERVICE_ROLE_KEY || "", {
+export const supabaseAnon = createClient<Database>(
+    process.env.SUPABASE_API_URL || "",
+    process.env.SUPABASE_ANON_SERVICE_ROLE_KEY || "",
+    {
         db: {
             schema: "public",
         },
         auth: {
             persistSession: false,
         },
-    });
-};
+    }
+);
+
+export const supabaseAdmin = createClient<Database>(
+    process.env.SUPABASE_API_URL || "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY || "",
+    {
+        db: {
+            schema: "public",
+        },
+        auth: {
+            persistSession: false,
+        },
+    }
+);
