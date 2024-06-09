a1188f76aad6da38a2a41b1bacaa77ab4d0594e9
Owan Hunte <owan@blockadjacent.com>
2024-06-09 19:43:55 -0400

    feat(algo-defi-core): add util fn to get Algorand limit orders

--- packages/algo-defi-core/src/index.ts
+++ packages/algo-defi-core/src/index.ts
@@ -1,5 +1,7 @@
+export type { TinymanClientWrapper, PactClientWrapper, AMMClients } from "./util/client";
+export type { AlgoLimitOrderAsset } from "./util/data-retriever";
+
 export { AlgoBots } from "./util/constants";
 export { getAMMClients } from "./util/client";
 export { signerWithSecretKey, optInToAsset, getAssetHolding } from "./util/foundation";
-
-export type { TinymanClientWrapper, PactClientWrapper, AMMClients } from "./util/client";
+export { getAlgoLimitOrders } from "./util/data-retriever";
---
+++ packages/algo-defi-core/src/util/data-retriever.ts
@@ -0,0 +1,48 @@
+import { supabaseAnon, Tables } from "@blockadjacent/bots-core";
+import { SupportedNetwork } from "@tinymanorg/tinyman-js-sdk";
+
+export const getAlgoLimitOrders = async (address: string, network: SupportedNetwork) => {
+    return supabaseAnon
+        .from("algo_limit_orders")
+        .select(
+            `
+            id,
+            network,
+            wallet_address,
+            order_type,
+            asset_in (
+                id,
+                token_id,
+                name,
+                unit_name,
+                decimals,
+                is_native,
+                is_supported
+            ),
+            asset_out (
+                id,
+                token_id,
+                name,
+                unit_name,
+                decimals,
+                is_native,
+                is_supported
+            ),
+            amount_in,
+            at_price,
+            slippage,
+            generate_reverse_trade,
+            reverse_trade_at_price,
+            first_asset_in_linked_trades,
+            always_use_starting_amount_in,
+            origin_trade
+        `
+        )
+        .eq("is_active", true)
+        .eq("is_completed", false)
+        .eq("wallet_address", address)
+        .eq("network", network)
+        .order("created_at", { ascending: true });
+};
+
+export type AlgoLimitOrderAsset = Omit<Tables<"algo_assets">, "created_at" | "updated_at" | "network">;
